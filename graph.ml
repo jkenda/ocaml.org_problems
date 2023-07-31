76336e98840875d657f32ebb104291e226e88998
Jakob Kenda <kenda.jakob@gmail.com>
2023-07-31 20:01:58 +0200

    BFS and DFS return path

--- graph.ml
+++ graph.ml
@@ -1,28 +1,42 @@
 type 'a node =
     Node of 'a * 'a node list
 ;;
+let get_el (Node (el, _)) = el;;
 
-let find_dfs start goal =
-    let rec search_node (Node (el, next)) =
+let find_dfs graph goal =
+    let rec search_node (Node (self, children)) =
         let rec search_list = function
-            | [] -> false
-            | hd :: tl -> search_node hd || search_list tl
+            | [] -> None
+            | hd :: tl ->
+                    match search_node hd with
+                    | None -> search_list tl
+                    | r -> r
         in
-        if el = goal then true
-        else search_list next
+        if self = goal then Some [self]
+        else
+            match search_list children with
+            | Some r -> Some (self :: r)
+            | None -> None
     in
-    search_node start
+    search_node graph
 ;;
-assert (find_dfs (Node ('a', [Node ('b', []); Node ('c', [])])) 'c');;
+assert (find_dfs (Node ('a', [Node ('b', []); Node ('c', [])])) 'h' = None);;
+assert (find_dfs (Node ('a', [Node ('b', []); Node ('c', [Node ('h', [])])])) 'h' = Some ['a'; 'c'; 'h']);;
 
-let find_bfs start goal =
-    let rec aux = function
-        | [] -> false
-        | Node (el, next) :: tl ->
-                if el = goal then true
-                else aux (tl @ next)
+let find_bfs graph goal =
+    let create_paths path =
+        List.map (fun (Node (child, _)) -> child :: path)
     in
-    aux [start]
+    let rec aux paths queue =
+        match paths, queue with
+        | [], [] -> None
+        | path :: paths, Node (self, children) :: queue ->
+                if self = goal then Some (List.rev path)
+                else aux (paths @ create_paths path children) (queue @ children)
+        | _, _ -> raise (Failure "unreachable")
+    in
+    aux [[get_el graph]] [graph]
 ;;
-assert (find_bfs (Node ('a', [Node ('b', []); Node ('c', [])])) 'c');;
+assert (find_bfs (Node ('a', [Node ('b', []); Node ('c', [])])) 'c' = Some ['a'; 'c']);;
+assert (find_bfs (Node ('a', [Node ('b', []); Node ('c', [Node ('h', [])])])) 'h' = Some ['a'; 'c'; 'h']);;
 
