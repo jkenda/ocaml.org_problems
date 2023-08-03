55dd00fd214f15dda9de6e37b718e7f7da34dee0
Jakob Kenda <kenda.jakob@gmail.com>
2023-08-03 18:34:20 +0200

    generalized all search functions

--- graph.ml
+++ graph.ml
@@ -1,13 +1,34 @@
-type distance = int;;
-type 'a node =
-    Node of 'a * ('a node * distance) list
-;;
-let get_val (Node (el, _)) = el;;
-let get_neigh (Node (_, neighbours)) = neighbours;;
+exception Illegal;;
+
+type dist = int;;
+type 'a graph = {
+    origin: 'a;
+    goals: 'a list;
+    neigh_f: 'a -> ('a * dist) list;
+    heur_f: 'a -> dist
+};;
+
+let graph1 = {
+    origin = 'a'; goals = ['h'];
+    neigh_f = (function 'a' -> ['b', 1; 'c', 2] | 'b' | 'c' -> [] | _ -> raise Illegal);
+    heur_f = (function _ -> 0)
+};;
+
+let graph2 = {
+    origin = 'a'; goals = ['c'];
+    neigh_f = (function 'a' -> ['b', 1; 'c', 2] | 'b' | 'c' -> [] | _ -> raise Illegal);
+    heur_f = (function _ -> 0)
+};;
+
+let graph3 = {
+    origin = 'a'; goals = ['h'];
+    neigh_f = (function 'a' -> ['b', 1; 'c', 2] | 'b' -> [] | 'c' -> ['h', 3] | _ -> raise Illegal);
+    heur_f = (function _ -> 0)
+}
 
 (* depth first search *)
-let find_dfs graph goals =
-    let rec search_node path sum (Node (self, children)) =
+let find_dfs { origin; goals; neigh_f; _ } =
+    let rec search_node path sum self =
         let rec search_list = function
             | [] -> None
             | (child, dist) :: tl ->
@@ -20,33 +41,34 @@ let find_dfs graph goals =
         (* prevent cycling *)
         else if List.mem self path then None
         (* search children *)
-        else search_list children
+        else search_list (neigh_f self)
     in
-    search_node [] 0 graph
+    search_node [] 0 origin
         |> Option.map (fun (path, distance) -> (List.rev path, distance))
 ;;
-assert (find_dfs (Node ('a', [Node ('b', []), 1; Node ('c', []), 2])) ['h'] = None);;
-assert (find_dfs (Node ('a', [Node ('b', []), 1; Node ('c', []), 2])) ['c'] = Some (['a'; 'c'], 2));;
-assert (find_dfs (Node ('a', [Node ('b', []), 1; Node ('c', [Node ('h', []), 3]), 2])) ['h'] = Some (['a'; 'c'; 'h'], 5));;
+assert (find_dfs graph1 = None);;
+assert (find_dfs graph2 = Some (['a'; 'c'], 2));;
+assert (find_dfs graph3 = Some (['a'; 'c'; 'h'], 5));;
 
 (* breadth first search *)
-let find_bfs graph goals =
-    let add_to_queue path sum children queue =
-        queue @ List.map (fun ((Node (tag, _), dist) as child) -> tag :: path, sum + dist, child) children
+let find_bfs { origin; goals; neigh_f; _ } =
+    let add_to_queue queue path sum children =
+        queue @ List.map (fun ((node, dist) as child) -> node :: path, sum + dist, child) children
     in
     let rec aux = function
         (* queue is exhausted, none of the goals found *)
         | [] -> None
         (* there are still elements in the queue *)
-        | (path, dist, (Node (self, children), _)) :: queue ->
+        | (path, dist, (self, _)) :: queue ->
                 (* goal found *)
                 if List.mem self goals then Some (List.rev path, dist)
-                else aux (add_to_queue path dist children queue)
+                else aux (add_to_queue queue path dist (neigh_f self))
     in
-    aux [([get_val graph], 0, (graph, 0))]
+    aux [([origin], 0, (origin, 0))]
 ;;
-assert (find_bfs (Node ('a', [Node ('b', []), 1; Node ('c', []), 2])) ['c'] = Some (['a'; 'c'], 2));;
-assert (find_bfs (Node ('a', [Node ('b', []), 1; Node ('c', [Node ('h', []), 3]), 2])) ['h'] = Some (['a'; 'c'; 'h'], 5));;
+assert (find_bfs graph1 = None);;
+assert (find_bfs graph2 = Some (['a'; 'c'], 2));;
+assert (find_bfs graph3 = Some (['a'; 'c'; 'h'], 5));;
 
 type 'a container =
     | Bottom
@@ -59,9 +81,9 @@ let container_map f = function
 ;;
 
 (* iterative deepeining depth first search *)
-let find_ids graph goals =
+let find_ids { origin; goals; neigh_f; _ } =
     let find_dfs_limited limit =
-        let rec search_node path sum (Node (self, children)) limit =
+        let rec search_node path sum self limit =
             let rec search_list = function
                 | [] -> Bottom
                 | (child, dist) :: tl ->
@@ -81,9 +103,9 @@ let find_ids graph goals =
             else if List.mem self path then Bottom
             (* search children *)
             else
-                search_list children
+                search_list (neigh_f self)
         in
-        search_node [] 0 graph limit
+        search_node [] 0 origin limit
             |> container_map (fun (path, distance) -> (List.rev path, distance))
     in
     let rec aux limit =
@@ -94,31 +116,34 @@ let find_ids graph goals =
     in
     aux 0
 ;;
-assert (find_ids (Node ('a', [Node ('b', []), 1; Node ('c', []), 2])) ['c'] = Some (['a'; 'c'], 2));;
-assert (find_ids (Node ('a', [Node ('b', []), 1; Node ('c', [Node ('h', []), 3]), 2])) ['h'] = Some (['a'; 'c'; 'h'], 5));;
+assert (find_ids graph1 = None);;
+assert (find_ids graph2 = Some (['a'; 'c'], 2));;
+assert (find_ids graph3 = Some (['a'; 'c'; 'h'], 5));;
 
 
-let rec graph = Node ('s', [a, 3; b, 2; c, 2])
-and a = Node ('a', [d, 3; e, 5])
-and b = Node ('b', [e, 4; f, 3; g, 3])
-and c = Node ('c', [g, 1; h, 5])
-and d = Node ('d', [i, 3; j, 1])
-and e = Node ('e', [j, 2; l, 1])
-and f = Node ('f', [m, 3; n, 1])
-and g = Node ('g', [])
-and h = Node ('h', [o, 2; p, 1])
-and i = Node ('i', [])
-and j = Node ('j', [k, 1])
-and k = Node ('k', [e, 1])
-and l = Node ('l', [])
-and m = Node ('m', [])
-and n = Node ('n', [o, 2])
-and o = Node ('o', [])
-and p = Node ('p', []);;
-
-let goals = ['l'; 'o'];;
-
-let get_dist (Node _, dist) = dist;;
+let graph = {
+    origin = 's'; goals = ['l'; 'o'];
+    neigh_f = (function
+        | 's' -> ['a', 3; 'b', 2; 'c', 2]
+        | 'a' -> ['d', 3; 'e', 5]
+        | 'b' -> ['e', 4; 'f', 3; 'g', 3]
+        | 'c' -> ['g', 1; 'h', 5]
+        | 'd' -> ['i', 3; 'j', 1]
+        | 'e' -> ['j', 2; 'l', 1]
+        | 'f' -> ['m', 3; 'n', 1]
+        | 'g' -> []
+        | 'h' -> ['o', 2; 'p', 1]
+        | 'i' -> []
+        | 'j' -> ['k', 1]
+        | 'k' -> ['e', 1]
+        | 'l' -> []
+        | 'm' -> []
+        | 'n' -> ['o', 2]
+        | 'o' -> []
+        | 'p' -> []
+        | _ -> raise (Failure "node doesn't exist"));
+    heur_f = (function _ -> raise (Failure "the default heur_f is a placeholder"))
+};;
 
 let correct_h = function 's' -> 4 | 'a' -> 5 | 'b' -> 5 | 'c' -> 5 | 'd' -> 1 | 'e' -> 1 | 'f' -> 3 | 'g' -> 6 | 'h' -> 2
                        | 'i' -> 1 | 'j' -> 2 | 'k' -> 1 | 'l' -> 0 | 'm' -> 6 | 'n' -> 2 | 'o' -> 0 | 'p' -> 1
@@ -132,22 +157,22 @@ let incorrect_h = function 's' -> 4 | 'a' -> 5 | 'b' -> 6 | 'c' -> 5 | 'd' -> 1 
                          | 'i' -> 1 | 'j' -> 2 | 'k' -> 1 | 'l' -> 2 | 'm' -> 6 | 'n' -> 2 | 'o' -> 0 | 'p' -> 1
                          | _ -> raise (Failure "invalid node reached");;
 
-assert (find_bfs graph goals = Some (['s'; 'a'; 'e'; 'l'], 9));;
-assert (find_dfs graph goals = Some (['s'; 'a'; 'd'; 'j'; 'k'; 'e'; 'l'], 10));;
-assert (find_ids graph goals = find_bfs graph ['l'; 'o'])
+assert (find_bfs graph = Some (['s'; 'a'; 'e'; 'l'], 9));;
+assert (find_dfs graph = Some (['s'; 'a'; 'd'; 'j'; 'k'; 'e'; 'l'], 10));;
+assert (find_ids graph = find_bfs graph);;
 
 
 (* greedy best-first search *)
-let find_greedy graph goals heuristic =
-    let rec search_node path sum (Node (self, children)) =
+let find_greedy { origin; goals; neigh_f; heur_f } =
+    let rec search_node path sum self =
         let search_min_child children =
-            let rec min ((min, _, _) as prev) (Node (tag, _) as hd, dist) =
-                let h = heuristic tag in
+            let rec min ((min, _, _) as prev) (hd, dist) =
+                let h = heur_f hd in
                 if h < min then (h, Some hd, dist) else prev
             in
             let (_, child, dist) = List.fold_left min (Int.max_int, None, 0) children in
             match child with
-            | Some (Node (tag, _) as child) -> search_node (self :: path) (sum + dist) child
+            | Some child -> search_node (self :: path) (sum + dist) child
             | None -> None
         in
         (* goal reached *)
@@ -155,23 +180,23 @@ let find_greedy graph goals heuristic =
         (* prevent cycling *)
         else if List.mem self path then None
         (* search children *)
-        else search_min_child children
+        else search_min_child (neigh_f self)
     in
-    search_node [] 0 graph
+    search_node [] 0 origin
         |> Option.map (fun (path, distance) -> (List.rev path, distance))
 ;;
-assert (find_greedy graph goals perfect_h = Some (['s'; 'b'; 'e'; 'l'], 7));;
-assert (find_greedy graph ['l'; 'o'] correct_h = None);;
+assert (find_greedy { graph with heur_f = perfect_h } = Some (['s'; 'b'; 'e'; 'l'], 7));;
+assert (find_greedy { graph with heur_f = correct_h } = None);;
 
 (* A* *)
-let find_a' graph goals neigh_f heur_f =
+let find_a' { origin; goals; neigh_f; heur_f } =
     let add_to_queue queue path sum neighbours =
-        let insert list ((n, dist) as node) =
-            let (dist, _, _) as node = sum + dist, node, (get_val n) :: path in
+        let insert list ((new_n, dist) as node) =
+            let (dist, _, _) as node = sum + dist, node, new_n :: path in
             let rec insert' = function
                 | [] -> [node]
-                | (d, (Node (t, _), _), _) as hd :: tl as ls ->
-                        if dist + heur_f (get_val n) <= d + heur_f t then node :: ls
+                | (d, (n, _), _) as hd :: tl as ls ->
+                        if dist + heur_f new_n <= d + heur_f n then node :: ls
                         else hd :: insert' tl
             in insert' list
         in
@@ -183,15 +208,15 @@ let find_a' graph goals neigh_f heur_f =
         (* there are still elements in the queue *)
         | (dist, (node, sum), path) :: queue ->
                 (* goal found *)
-                if List.mem (get_val node) goals then Some (List.rev path, dist)
+                if List.mem node goals then Some (List.rev path, dist)
                 else aux (add_to_queue queue path dist (neigh_f node))
     in
-    aux [(0, (graph, 0), [get_val graph])]
+    aux [(0, (origin, 0), [origin])]
 ;;
-assert (find_a' graph goals get_neigh correct_h = Some (['s'; 'b'; 'e'; 'l'], 7));;
-assert (find_a' graph goals get_neigh perfect_h = find_a' graph goals get_neigh correct_h);;
+assert (find_a' { graph with heur_f = correct_h } = Some (['s'; 'b'; 'e'; 'l'], 7));;
+assert (find_a' { graph with heur_f = perfect_h } = find_a' { graph with heur_f = correct_h });;
 
-assert (find_a' graph goals get_neigh incorrect_h = Some (['s'; 'b'; 'f'; 'n'; 'o'], 8));;
+assert (find_a' { graph with heur_f = incorrect_h } = Some (['s'; 'b'; 'f'; 'n'; 'o'], 8));;
 
 type ('a, 'b) result =
     | Result of 'a
@@ -199,7 +224,7 @@ type ('a, 'b) result =
 ;;
 
 (* IDA* *)
-let find_ida' graph goals heuristic =
+let find_ida' { origin; goals; neigh_f; heur_f } =
     let find_a'limited limit =
         (* smallest of the distances that go over the limit *)
         let next_limit = ref None in
@@ -209,13 +234,13 @@ let find_ida' graph goals heuristic =
             | Some nl -> if limit < nl then next_limit := Some limit
         in
         (* add children's properties to queue *)
-        let add_to_queue path sum children queue =
-            let insert ((Node (tag, _), dist) as node) list =
-                let (dist, _, _) as node = sum + dist, node, tag :: path in
-                let dist_plus_h = dist + heuristic tag in
+        let add_to_queue queue path sum children =
+            let insert ((new_node, dist) as node) list =
+                let (dist, _, _) as node = sum + dist, node, new_node :: path in
+                let dist_plus_h = dist + heur_f new_node in
                 let rec insert' = function
                     | [] -> [node]
-                    | (d, (Node (t, _), _), _) :: _ as ls when dist_plus_h <= d + heuristic t -> node :: ls
+                    | (d, (n, _), _) :: _ as ls when dist_plus_h <= d + heur_f n -> node :: ls
                     | hd :: tl -> hd :: insert' tl
                 in
                 if dist_plus_h > limit then (next_limit_is dist_plus_h; list)
@@ -227,12 +252,12 @@ let find_ida' graph goals heuristic =
             (* queue is exhausted, none of the goals found *)
             | [] -> Limit !next_limit
             (* there are still elements in the queue *)
-            | (dist, (Node (tag, children), _), path) :: queue ->
+            | (dist, (self, _), path) :: queue ->
                     (* goal found *)
-                    if List.mem tag goals then Result (List.rev path, dist)
-                    else aux (add_to_queue path dist children queue)
+                    if List.mem self goals then Result (List.rev path, dist)
+                    else aux (add_to_queue queue path dist (neigh_f self))
         in
-        aux [(0, (graph, 0), [get_val graph])]
+        aux [(0, (origin, 0), [origin])]
     in
     let rec find' limit =
         match find_a'limited limit with
@@ -240,16 +265,12 @@ let find_ida' graph goals heuristic =
         | Limit None -> None
         | Limit (Some limit) -> find' limit
     in
-    get_val graph
-        |> heuristic
-        |> find'
+    find' (heur_f origin)
 ;;
-assert (find_ida' graph goals incorrect_h = Some (['s'; 'b'; 'f'; 'n'; 'o'], 8));;
-assert (find_ida' graph goals correct_h   = Some (['s'; 'b'; 'e'; 'l'], 7));;
-assert (find_ida' graph goals perfect_h   = find_ida' graph goals correct_h);;
-
-assert (find_ida' graph goals incorrect_h = find_a' graph goals get_neigh incorrect_h);;
-assert (find_ida' graph goals correct_h   = find_a' graph goals get_neigh correct_h);;
-assert (find_ida' graph goals perfect_h   = find_a' graph goals get_neigh perfect_h);;
-
+assert (find_ida' { graph with heur_f = incorrect_h } = Some (['s'; 'b'; 'f'; 'n'; 'o'], 8));;
+assert (find_ida' { graph with heur_f = correct_h   } = Some (['s'; 'b'; 'e'; 'l'], 7));;
+assert (find_ida' { graph with heur_f = perfect_h   } = find_ida' { graph with heur_f = correct_h });;
+assert (find_ida' { graph with heur_f = incorrect_h } = find_a' { graph with heur_f = incorrect_h });;
+assert (find_ida' { graph with heur_f = correct_h   } = find_a' { graph with heur_f = correct_h   });;
+assert (find_ida' { graph with heur_f = perfect_h   } = find_a' { graph with heur_f = perfect_h   });;
 
